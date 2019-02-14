345af3672957dc97d489f4918c5389e826c0f9c1
Kuba <jakubstankowski@protonmail.com>
2019-02-14 11:08:14 +0100

    CHANGED CODE

--- INTERMEDIATE ALGORITHM SCRIPTING/Map the Debris.js
+++ INTERMEDIATE ALGORITHM SCRIPTING/Map the Debris.js
@@ -3,14 +3,14 @@ function orbitalPeriod(arr) {
     var GM = 398600.4418;
     var earthRadius = 6367.4447;
 
-    const calculateOrbitalPeriod = avgAlt => {
+    const calculateOrbitalPeriod = ((avgAlt) => {
         const addEarthRadiusAvgAlt = avgAlt + earthRadius;
         const powEarthRadiusAvgAlt = Math.pow(addEarthRadiusAvgAlt, 3);
         const splitEarthRadiuAvgAltWithGM = powEarthRadiusAvgAlt / GM;
         const sqrtAllEforts = Math.sqrt(splitEarthRadiuAvgAltWithGM);
         const countOrbitalPeriod = 2 * Math.PI * sqrtAllEforts;
         return Math.round(countOrbitalPeriod);
-    };
+    });
 
     for (let val in arr) {
         // console.log(arr[val])
